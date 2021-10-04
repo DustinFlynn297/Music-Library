568a04a3e943d247edf63c2f59bb0bb7935e3412
Dustin Flynn <flynnd297@gmail.com>
2021-10-04 14:02:33 -0500

    Added Debug Configuration for Django.

--- music_project/library/views.py
+++ music_project/library/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 # Create your views here.
-class Songlist(APIView):
+class SongList(APIView):
 
     def get(self, request):
         song = Song.objects.all()
